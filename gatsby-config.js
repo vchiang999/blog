674c79e09e7ffcd2e80f0277142a0d4327976832
VC <vc@VC-MBP.local>
2023-08-23 15:32:51 +0100

    correct google analytics script

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,25 +28,10 @@ module.exports = {
         trackingIds: [
           "G-G10S02JQCL", // Google Analytics / GA
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "YOUR_SELF_HOSTED_ORIGIN",
-          // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
         },
       },
     },
